cb39e04a04b562d1074c4865ccfc7323380def12
Jaewook Kim <yearsingle@naver.com>
2025-02-28 14:08:30 +0900

    change project to react-vite

--- usakura/src/App.tsx
+++ usakura/src/App.tsx
@@ -1,22 +1,35 @@
-import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './routes/routes';
-import Navbar from './components/Navbar';
-import Social from './components/Social';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter>
-      <div className="layout-container">
-        <div className="side-container left"></div>
-        <div className="app-container">
-          <AppRoutes />
-          <Navbar />
-        </div>
-        <div className="side-container right"><Social /></div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </BrowserRouter>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
